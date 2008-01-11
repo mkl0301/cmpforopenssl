85570d3f9efdc7873e74af067f2ce8fa831f09ad
mpeylo <mpeylo@8c94e50a-443e-0410-be32-d2e341db1201>
2008-01-11 01:16:27 +0000

    added script for KUR with izac.de:8080
    git-svn-id: svn://svn.code.sf.net/p/cmpforopenssl/code@19 8c94e50a-443e-0410-be32-d2e341db1201

---
+++ scripting/do_ossl_kur_izac.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+myDir=`dirname $0`
+. $myDir/settings.sh
+
+if [ "$1" == "-help" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+	echo "Usage: $0"
+	exit 1
+fi
+
+set -x
+
+${CMPCLIENT} --kur \
+             --server ${IZAC_SERVER} \
+						 --cacert ${IZAC_CACERT} \
+						 --key ${CLKEY} \
+						 --newkey ${NEWCLKEY} \
+						 --clcert ${CLCERT} \
+						 --newclcert ${NEWCLCERT}
+set +x
