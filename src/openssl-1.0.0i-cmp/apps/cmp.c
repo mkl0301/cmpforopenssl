d7ee8df3eb265d7473466c47b42becb0810cfe2d
mviljane <mviljane@8c94e50a-443e-0410-be32-d2e341db1201>
2012-06-13 12:43:38 +0000

    add cmdline option parsing for the cmp command
    git-svn-id: svn://svn.code.sf.net/p/cmpforopenssl/code@395 8c94e50a-443e-0410-be32-d2e341db1201

--- src/openssl-1.0.0i-cmp/apps/cmp.c
+++ src/openssl-1.0.0i-cmp/apps/cmp.c
@@ -75,16 +75,66 @@
 #undef PROG
 #define PROG	cmp_main
 
-static const char *cmp_usage[]={
-    "usage: cmp args",
-    "",
-    "  -server arg - address of the cmp server",
-    " .....",
-    ""
+typedef enum { OPT_BOOL, OPT_NUM, OPT_TXT } opttype_t;
+typedef struct 
+    {
+    const char *name;
+    const char *help;
+    opttype_t type;
+    union {
+        char **txt;
+        int *num;
+        } v;
+    } opt_t;
+
+static char *opt_server=NULL;
+static char *opt_cmd=NULL;
+static char *opt_user=NULL;
+static char *opt_pass=NULL;
+
+static opt_t cmp_opts[]={
+    { "server", "The 'ADDRESS:PORT/PATH' for the CMP server.", OPT_TXT, {&opt_server} },
+    { "cmd", "CMP command to execute: ir/kur/cr/rr/ckuann/...", OPT_TXT, {&opt_cmd} },
+    { "user", "Username for doing the IR with a pre-shared key.", OPT_TXT, {&opt_user} },
+    { "pass", "Password for doing the IR with a pre-shared key.", OPT_TXT, {&opt_pass} },
+
+#if 0
+    { "cert", "Client's current certificate.", OPT_TXT, {&} },
+    { "key", "Key for the client's current certificate.", OPT_TXT, {&} },
+    { "keypass", "Password for the key.", OPT_TXT, {&} },
+
+    { "cert", "Client's current certificate.", OPT_TXT, {&} },
+    { "newkey", "", OPT_TXT, {&} },
+    { "newkeypass", "", OPT_TXT, {&} },
+
+    { "certout", "Where to save the new certificate", OPT_TXT, {&} },
+    
+
+    { "CApath", "A directory of trusted certificates.", OPT_TXT, {&} },
+    { "CAfile", "A file of trusted certificates.", OPT_TXT, {&} },
+
+    { "validate_chain", ".", OPT_TXT, {&} },
+#endif
 };
 
+void show_help(void)
+    {
+    const int ALIGN_COL=8;
+    opt_t *o=cmp_opts;
+    int i=0,j=0;
+    
+    BIO_puts(bio_err, "\nusage: cmp args\n");
+    for (i=0; i < sizeof(cmp_opts)/sizeof(cmp_opts[0]); i++,o++) {
+        BIO_printf(bio_err, "  -%s", o->name);
+        for (j=ALIGN_COL-strlen(o->name); j > 0; j--)
+            BIO_puts(bio_err, " ");
+        BIO_printf(bio_err, "- %s\n", o->help);
+        }
+    }
+
+
 static CONF *conf=NULL;
-static CONF *extconf=NULL;
+/* static CONF *extconf=NULL; */
 static BIO *bio_c_out=NULL;
 
 int MAIN(int argc, char **argv)
@@ -130,16 +180,56 @@ int MAIN(int argc, char **argv)
 		OPENSSL_free(tofree);
 		tofree = NULL;
 		}
-    
-    badops=1;
-    if (badops)
+
+    while (--argc > 0 && ++argv)
         {
-        int i;
-        for (i=0; i < sizeof(cmp_usage)/sizeof(cmp_usage[0]); i++)
-            BIO_printf(bio_err, "%s\n", cmp_usage[i]);
+        opt_t *opt=cmp_opts;
+        char *arg=*argv;
+        int found=0,i;
+
+        if (*arg++ != '-' || *arg == 0) {
+            badops=1;
+            break;
+        }
+        
+        for (i=0; i < sizeof(cmp_opts)/sizeof(cmp_opts[0]); i++,opt++) {
+            if (!strcmp(arg, opt->name))
+                {
+                if (argc <= 1 && opt->type != OPT_BOOL) {
+                    BIO_printf(bio_err, "missing argument for '-%s'\n", opt->name);
+                    badops=1;
+                    goto bad_ops;
+                    }
+                switch(opt->type)
+                    {
+                    case OPT_BOOL:
+                        *opt->v.num = 1;
+                        break;
+                    case OPT_NUM:
+                        *opt->v.num = atoi(*++argv);
+                        argc--;
+                        break;
+                    case OPT_TXT:
+                        *opt->v.txt = *++argv;
+                        argc--;
+                        break;
+                    default:
+                        badops=1;
+                        break;
+                    }
+                found=1;
+                }
+            if (!found) {
+                BIO_printf(bio_err, "unknown argument: '%s'\n", *argv);
+                badops=1;
+                goto bad_ops;
+                }
+            }
         }
 
-    BIO_printf(bio_c_out,"not implemented yet\n");
+bad_ops:
+    if (badops)
+        show_help();
 
 
     ret=0;
