1252a6cc6aa4ab2df4b6a4fcd9ef26445ad2ca8a
mviljane <mviljane@8c94e50a-443e-0410-be32-d2e341db1201>
2012-07-12 11:29:33 +0000

    add cmp command to progs.h
    git-svn-id: svn://svn.code.sf.net/p/cmpforopenssl/code@432 8c94e50a-443e-0410-be32-d2e341db1201

--- src/openssl-1.0.0i-cmp/apps/progs.h
+++ src/openssl-1.0.0i-cmp/apps/progs.h
@@ -46,6 +46,7 @@ extern int engine_main(int argc,char *argv[]);
 extern int ocsp_main(int argc,char *argv[]);
 extern int prime_main(int argc,char *argv[]);
 extern int ts_main(int argc,char *argv[]);
+extern int cmp_main(int argc,char *argv[]);
 
 #define FUNC_TYPE_GENERAL	1
 #define FUNC_TYPE_MD		2
@@ -147,6 +148,7 @@ FUNCTION functions[] = {
 #endif
 	{FUNC_TYPE_GENERAL,"prime",prime_main},
 	{FUNC_TYPE_GENERAL,"ts",ts_main},
+	{FUNC_TYPE_GENERAL,"cmp",cmp_main},
 #ifndef OPENSSL_NO_MD2
 	{FUNC_TYPE_MD,"md2",dgst_main},
 #endif
